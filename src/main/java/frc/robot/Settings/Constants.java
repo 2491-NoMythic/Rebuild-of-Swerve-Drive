7121e79676728754b6251c06772d74dc563771ba
NoMythic <2491NoMythic@gmail.com>
2024-11-26 20:06:08 -0600

    Less redesign than planned

--- src/main/java/frc/robot/Settings/Constants.java
+++ src/main/java/frc/robot/Settings/Constants.java
@@ -1,9 +1,51 @@
 package frc.robot.Settings;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public final class Constants {
     private Constants() {}
+    public static final class HardwareConstants{
+        public static final double DRIVETRAIN_WHEEL_DIAMETER = 2491;
+    }
+    public static final class IDConstants{
+        public static final int FL_DRIVE_MOTOR_ID = 1;
+        public static final int FL_STEER_MOTOR_ID = 2;
+        public static final int FL_STEER_ENCODER_ID = 1;
+        public static final Rotation2d FL_STEER_OFFSET = Rotation2d.fromRotations(0);
 
+        public static final int FR_DRIVE_MOTOR_ID = 3;
+        public static final int FR_STEER_MOTOR_ID = 4;
+        public static final int FR_STEER_ENCODER_ID = 2;
+        public static final Rotation2d FR_STEER_OFFSET = Rotation2d.fromRotations(0);
+
+        public static final int BL_DRIVE_MOTOR_ID = 5;
+        public static final int BL_STEER_MOTOR_ID = 6;
+        public static final int BL_STEER_ENCODER_ID = 3;
+        public static final Rotation2d BL_STEER_OFFSET = Rotation2d.fromRotations(0);
+
+        public static final int BR_DRIVE_MOTOR_ID = 7;
+        public static final int BR_STEER_MOTOR_ID = 8;
+        public static final int BR_STEER_ENCODER_ID = 4;
+        public static final Rotation2d BR_STEER_OFFSET = Rotation2d.fromRotations(0);
+    }
     public static final class ModuleConstants{
-        
+        public static final double DEGREES_TO_ROTATIONS = 2491;
+        public static final double DRIVETRAIN_ROTATIONS_TO_METERS = (HardwareConstants.DRIVETRAIN_WHEEL_DIAMETER * Math.PI);
+    }
+    public static final class CTREConfigs {
+        public TalonFXConfiguration driveMotorConfig;
+        public TalonFXConfiguration steerMotorConfig;
+        public CANcoderConfiguration steerEncoderConfig;
+        public Pigeon2Configuration pigeon2Config;
+
+        public CTREConfigs() {
+          driveMotorConfig = new TalonFXConfiguration();
+          steerMotorConfig = new TalonFXConfiguration();
+          steerEncoderConfig = new CANcoderConfiguration();
+        } 
     }
 }
--- src/main/java/frc/robot/Subsystems/SwerveModule.java
+++ src/main/java/frc/robot/Subsystems/SwerveModule.java
@@ -2,23 +2,66 @@ package frc.robot.Subsystems;
 import com.ctre.phoenix6.configs.*;
 import com.ctre.phoenix6.controls.*;
 import com.ctre.phoenix6.hardware.*;
-
+import frc.robot.Settings.Constants.ModuleConstants;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 public class SwerveModule {
+    //The mechanical bits.
     private final TalonFX drive_Motor;
     private final TalonFX steer_Motor;
     private final CANcoder steer_Encoder;
-    private final Rotation2d steer_offset;
+    private final Rotation2d steer_Offset;
+    private PositionDutyCycle steer_Direction = new PositionDutyCycle(0);
+    private double desAngle;
+    
      
   /**
-   * Constructs a SwerveModule with a drive motor, turning motor and turning encoder.
+   * Constructs a SwerveModule with a drive motor, turning motor and turning encoder. Also takes the wheel offset for directions.
    *
-   * @param driveMotorChannel 
-   * @param steerMotorChannel 
-   * @param steerEncoderChannel
-   * @param steerEncoderOffset
+   * @param driveID 
+   * @param steerID 
+   * @param encID
+   * @param Offler (offset, rotation2d)
    */
-  public SwerveModule(String moduleName, int driveID, int steerID, int encID, Rotation2d OFFLER){
-    
+  public SwerveModule(String moduleName, int driveID, int steerID, int encID, Rotation2d Offler){
+    drive_Motor = new TalonFX(driveID);
+    steer_Motor = new TalonFX(steerID);
+    steer_Encoder = new CANcoder(encID);
+    steer_Offset = Offler;
+  
   }
+  //This section is the 'direct get' section. Everything that gets something directly from a motor is in here. 
+  /**
+   * Returns the current encoder angle of the steer motor.
+   * @return The current encoder angle of the steer motor.
+   */
+  public Rotation2d getRotation() {
+    return Rotation2d.fromRotations(MathUtil.inputModulus(steer_Motor.getPosition().getValue(), -0.5, 0.5));
+  }
+  /**
+   * Returns the current encoder velocity of the drive motor.
+   * @return The current velocity of the drive motor in meters/second.
+   */
+  public double getSpeedMetersPerSecond() {
+    return (drive_Motor.getVelocity().getValue() * ModuleConstants.DRIVETRAIN_ROTATIONS_TO_METERS);
+  }
+  /**finds the curernt encoder position, it removes the current offset so we just get the raw position
+   * @return
+   */
+    public double findOffset() {
+      return MathUtil.inputModulus(
+        (steer_Encoder.getPosition().getValue()+steer_Offset.getRotations()),
+        -0.5,
+        0.5);
+    }
+  //This is the get new section. It recives instructions. 
+  /**
+   * Returns the target angle of the wheel.
+   * @return The target angle of the wheel in degrees.
+   */
+  public double getTargetAngle() {
+    return desAngle;
+  }
+  //This is the state section. It controls the actual swerve drive states. 
+
 }
